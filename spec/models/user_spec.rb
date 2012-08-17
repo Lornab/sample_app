6138dbd4496e77de2f2dc058341cab602df793bd
Lorna <lorna@clicknmix.co.uk>
2012-08-17 12:12:41 +0100

    make basic User Model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,7 @@
 
 require 'spec_helper'
 
-describe User do
+  describe User do
 
   before do
     @user = User.new(name: "Example User", email: "user@example.com", 
@@ -37,48 +37,57 @@ describe User do
     it { should_not be_valid }
   end
 
-    describe "when name is too long" do
+  describe "when name is too long" do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
 
-    describe "when email is not present" do
+  describe "when email is not present" do
     before { @user.email = " " }
     it { should_not be_valid }
   end
 
-    describe "when email format is invalid" do
+  describe "when email format is invalid" do
     it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
-      addresses.each do |invalid_address|
-        @user.email = invalid_address
-        @user.should_not be_valid
-      end      
-    end
+    addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                foo@bar_baz.com foo@bar+baz.com]
+    addresses.each do |invalid_address|
+    @user.email = invalid_address
+    @user.should_not be_valid
+    end      
   end
+end
 
   describe "when email format is valid" do
     it "should be valid" do
-      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-      addresses.each do |valid_address|
-        @user.email = valid_address
-        @user.should be_valid
-      end      
-    end
+    addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+    addresses.each do |valid_address|
+    @user.email = valid_address
+    @user.should be_valid
+    end      
   end
+end
 
- describe "when email address is already taken" do
-    before do
-      user_with_same_email = @user.dup
-      user_with_same_email.email = @user.email.upcase
-      user_with_same_email.save
+  describe "when email address is already taken" do
+  before do
+    user_with_same_email = @user.dup
+    user_with_same_email.email = @user.email.upcase
+    user_with_same_email.save
     end
       
-  it { should_not be_valid }
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+    it { should_not be_valid }
   end
 
-    describe "when password is not present" do
+  describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
   end
